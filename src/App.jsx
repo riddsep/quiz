07ebaaa47f6588c2601bbff364808ec0b32968e1
riddsep <ridoseptiawan00@gmail.com>
2024-12-19 21:48:36 +0700

    initialize project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./Header";
+import Main from "./main";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 }
